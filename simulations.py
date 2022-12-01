de610dac970a66518367ad16f7ae2e2fa74c72e4
georgesuhk <georgesuhk@gmail.com>
2022-12-01 15:59:30 +0000

    added RKF45 method

--- simulations.py
+++ simulations.py
@@ -4,37 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-def KdeV_spatial(U, h, i, time_step):
-    """
-    Performs the spatial derivatives of the KdeV PDE
-    
-    :param U: All the U values at a particular point in time
-    :param h: spatial step
-    :param i: particular point in space
-    :return: Derivative evaluated at that spatial point i
-    """
-    #for periodic boundary conditions: #questionable
 
-    Uplus1 = U[(i+1) % len(U)]
-    
-    Umin1  = U[(i-1) % len(U)]
-    
-    Uplus2 = U[(i+2) % len(U)]
-    
-    Umin2  = U[(i-2) % len(U)]
-
-
-    print("+ ",Uplus1)
-    print("- ",Umin1)
-    #print((Uplus1**2 - Umin1**2))
-    #print((Uplus2 - 2*Uplus1 + 2*Umin1 - Umin2))
-    
-
-    out = 1/(4*h) * (Uplus1**2 - Umin1**2) * time_step + 1/(2*h**3) * (Uplus2 - 2*Uplus1 + 2*Umin1 - Umin2) * time_step
-
-    
-    return out
-
+#Initial conditions ----------------------------------------------------------
 def analytic(X, a, t):
     """
     Generates the initial condition
@@ -132,114 +103,7 @@ def analytic_solution(X, a=0.5, n_steps=100, dt=0.01):
     
     return [time_list, Un_list]
         
-
-
-
-def RK2(t_lims, U_0, time_step, rk_alpha, h):
-    #initial conditions
-    t = t_lims[0]
-    steps = 0
-    Un = U_0
-
-    #list for storing points in time
-    time_list = [t]
-    Un_list = [Un]
-    Un_snapshot = [Un[2]]
-    
-    #progress update
-    
-    print("RK Started")
-
-    while t < t_lims[1]:
-        Un_1 = np.zeros(len(U_0))
-        #propagating each point through time
-        for i in range(0, len(U_0)):
-            fa = -1 * KdeV_spatial(Un, h, i)
-            U_predictor = Un + (rk_alpha * fa * time_step)
-
-            fb = -1 * KdeV_spatial(U_predictor, h, i)
-            #print(1/2*rk_alpha * ((2*rk_alpha -1)*fa + fb) * time_step)
-            Un_1[i] = Un[i] + 1/(2*rk_alpha) * ((2*rk_alpha-1)*fa + fb) * time_step
-            
-        steps += 1
-        t = t + time_step
-        if steps % 10 == 0:
-        
-                time_list.append(t)
-                Un_list.append(Un_1)
-                Un_snapshot.append(Un_1[2])
-
-        Un = Un_1
-        
-      
-    print("RK Complete")
-
-    return [np.array(time_list), np.array(Un_list)]
-
-
-
-
-def RK4(t_lims, U_0, n_steps, time_step, rk_alpha, h):
-    #initial conditions
-    
-    t = t_lims[0]
-    #n_steps = round((t_lims[1]-t_lims[0])/time_step)
-    
-    steps = 0
-    Un = U_0
-
-    #list for storing points in time
-    time_list = [t]
-    Un_list = [Un]
-    Un_snapshot = [Un[2]]
-    
-
-
-    print("RK Started")
-
-    #while t < t_lims[1]:
-    while steps < n_steps:
-        Un_1 = np.zeros(len(U_0))
-        #propagating each point through time
-        for i in range(0, len(U_0)):
-            fa = -1 * KdeV_spatial(Un, h, i, time_step)
-            
-            U_predictor = Un + (0.5 * fa)
-            fb = -1 * KdeV_spatial(U_predictor, h, i, time_step)
-            
-            U_predictor = Un + (0.5 * fb)
-            fc = -1 * KdeV_spatial(U_predictor, h, i, time_step)
-                
-            U_predictor = Un + fc
-            fd = -1 * KdeV_spatial(U_predictor, h, i, time_step)
-
-            Un_1[i] = Un[i] + 1/6 * (fa + 2*fb + 2*fc + fd)
-            
-        steps += 1
-        t = t + time_step
-        #if steps % round(n_steps/100) == 0:
-        if 1==1:
-        
-            time_list.append(t)
-
-            Un_list.append(Un_1)
-            Un_snapshot.append(Un_1[2])
-
-            
-        if steps == round(n_steps/4):
-            print("25% Complete")
-        elif steps == round(n_steps/2):
-            print("50% Complete")
-        elif steps == round(n_steps * 3/4):
-            print("75% Complete")
-            
-        Un = Un_1
-        
-      
-    print("RK Complete")
-
-    return [np.array(time_list), np.array(Un_list), Un_snapshot]
-
+#FD Spatial derivatives -------------------------------------------------------
 
 #discretised FDE for the normal KdeV equation
 def KdeV_spatial_vec(U, h):
@@ -362,5 +226,68 @@ def RK4_shock_damped(U_0, n_steps=100, dt=0.001, h=0.1, D=1.0):
       
     print("RK Complete")
     return [np.array(time_list), np.array(Un_list)]
+
+
+#butcher tableau coefficients for RKF45
+k2_coeffs = [0.25]
+k3_coeffs = [3/32, 9/32]
+k4_coeffs = [1932/2197, -7200/2197, 7296/2197]
+k5_coeffs = [439/216, -8, 3680/513, -845/4104]
+k6_coeffs = [-8/27, 2, -3544/2565, 1859/4104, -11/40]
+
+RKF4_coeffs = [25/216, 0, 1408/2565, 2197/4104, -1/5, 0]
+RKF5_coeffs = [16/135, 0, 6656/12825, 28561/56430, -9/50, 2/55]
+
+
+
+#Embedded RKF (Felhberg) 45 Method
+def RKF45_vec(U_0, n_steps=100, dt=0.001, h=0.1, error_tol = 0.01):
+    #initial conditions
+    t = 0
+    steps = 0
+    Un = U_0
+    
+
+    #list for storing points in time
+    time_list = [t]
+    Un_list = [Un]
+    
+    print("RK Started")
+    #propagating each point through time
+    while steps < n_steps:
+        #checking for appropriate step size
+        U_diff = 1e5
+        while U_diff > error_tol:
+        
+            k1 = KdeV_spatial_vec(Un, h)*dt
+            k2 = KdeV_spatial_vec(Un + k2_coeffs[0]*k1, h)*dt
+            k3 = KdeV_spatial_vec(Un + k3_coeffs[0]*k1 + k3_coeffs[1]*k2, h)*dt
+            k4 = KdeV_spatial_vec(Un + k4_coeffs[0]*k1 + k4_coeffs[1]*k2 + k4_coeffs[2]*k3, h)*dt
+            k5 = KdeV_spatial_vec(Un + k5_coeffs[0]*k1 + k5_coeffs[1]*k2 + k5_coeffs[2]*k3 + k5_coeffs[3]*k4, h)*dt
+            k6 = KdeV_spatial_vec(Un + k6_coeffs[0]*k1 + k6_coeffs[1]*k2 + k6_coeffs[2]*k3 + k6_coeffs[3]*k4 + k6_coeffs[4]*k5, h)*dt
+            
+            U1_Order4 = Un + RKF4_coeffs[0]*k1 + RKF4_coeffs[2]*k3 + RKF4_coeffs[3]*k4 + RKF4_coeffs[4]*k5
+            
+            U1_Order5 = Un + RKF5_coeffs[0]*k1 + RKF5_coeffs[2]*k3 + RKF5_coeffs[3]*k4 + RKF5_coeffs[4]*k5 + RKF5_coeffs[5]*k6
+            
+            U_diff = np.max(U1_Order5 - U1_Order4)
+            
+            s = 0.84 * (error_tol/U_diff)**0.25
+            dt = s * dt
+            #print(dt)
+        
+        #passed step size check, completing step
+        steps += 1
+        t = t + dt
+        
+        Un = U1_Order5
+        Un_list.append(Un)
+        time_list.append(t)
+
+      
+    print("RK Complete")
+    return [np.array(time_list), np.array(Un_list)]
+
+
 
 
